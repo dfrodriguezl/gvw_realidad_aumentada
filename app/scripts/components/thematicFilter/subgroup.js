5467f229dc90e76e6e21bfafb462a88cbac715b0
karenmbuitrago1 <karenmbuitrago@gmail.com>
2022-09-13 14:57:43 -0500

    ajuste modal producto

--- app/scripts/components/thematicFilter/subgroup.js
+++ app/scripts/components/thematicFilter/subgroup.js
@@ -1,7 +1,5 @@
 // ACORDION SUBGRUPO - CONFIGURACION Y MAQUETA DE ACORDION PARA QUE MUESTRE LAS SUBGRUPO DEL GRUPO DESDE BASE DE DATOS
 import React, { useState } from "react";
-import RemoveIcon from '@material-ui/icons/Remove';
-import AddIcon from '@material-ui/icons/Add';
 
 
 const Accordion = (tematica) => {
@@ -18,21 +16,15 @@ const Accordion = (tematica) => {
       className={`filter__thematic__item ${item[0].COD_SUBGRUPO} ${ariaExpanded}`}
       key={item[0].COD_SUBGRUPO}
       id={item[0].COD_SUBGRUPO}
-      onClick={(e) => {
-        if (e.currentTarget.classList.length < 3) {
+      onClick={(e) => { 
+        if(e.currentTarget.classList.length < 3){
           setActiveIndex(item[0].COD_SUBGRUPO);
-        } else {
+        }else{
           setActiveIndex("");
         }
       }}
     >
-      <p className="filter__thematic__nameGroup">  {tematicaDos.GRUPOS[(item[0].COD_SUBGRUPO).substring(0, 3)][0].GRUPO}
-        <span style={{ float: 'right' }}>
-          {activeIndex !== item[0].COD_SUBGRUPO ?
-            <RemoveIcon fontSize="small" /> :
-            <AddIcon fontSize="small" />}
-        </span>
-      </p>
+      <p className="filter__thematic__nameGroup">  {tematicaDos.GRUPOS[(item[0].COD_SUBGRUPO).substring(0, 3)][0].GRUPO} </p>
       <h3 className="filter__thematic__name"> {item[0].SUBGRUPO}</h3>
       <ul
         key={item[0].COD_SUBGRUPO} id={item[0].COD_SUBGRUPO}
@@ -40,7 +32,6 @@ const Accordion = (tematica) => {
       >
         {liTemas}
       </ul>
-
     </li>
   );
 };
