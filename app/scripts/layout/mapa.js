3e984599956ff189440077768c08acd2f3b32deb
Diego <dfrodriguezl94@gmail.com>
2022-10-04 13:54:42 -0500

    Ajustes popup

--- app/scripts/layout/mapa.js
+++ app/scripts/layout/mapa.js
@@ -500,12 +500,9 @@ const Mapa = () => {
                 HTML += '<p class="popup__list"><span class="popup__value">' + parseFloat(dataPopup[variables.alias2]).toLocaleString("de-De").replace(",", ".") + '</span><span class="popup__valueItem">' + unidadesAbsolutas + '</span></p>';
               } else {
                 HTML += '<p class="popup__list"><span class="popup__value">' + parseFloat(dataPopup[0][variables.alias]).toLocaleString("de-De").replace(",", ".") + '</span><span class="popup__valueItem"> ' + unidadesAbsolutas + '</span></p>';
-                HTML += '<p class="popup__list"><span class="popup__subtitle">Precio promedio anterior</span></p>';
-                HTML += '<p class="popup__list"><span class="popup__value">' + parseFloat(dataPopup[0]["PRECIO_PROMEDIO_ANTERIOR"]).toLocaleString("de-De").replace(",", ".") + '</span><span class="popup__valueItem"> ' + unidadesAbsolutas + '</span></p>';
-                HTML += '<p class="popup__list"><span class="popup__subtitle">Tendencia</span></p>';
-                HTML += '<p class="popup__list"><span class="popup__value">' + dataPopup[0]["TENDENCIA"] + '</span></p>';
-                HTML += '<p class="popup__list"><span class="popup__subtitle">Variación</span></p>';
-                HTML += '<p class="popup__list"><span class="popup__value">' + parseFloat(dataPopup[0]["VARIACION_PP"]).toLocaleString("de-De").replace(",", ".") + '</span><span class="popup__valueItem">%</span></p>';
+                HTML += '<p class="popup__list"><span class="popup__subtitle">Precio promedio anterior: </span><span class="popup__subtitle">' + unidadesAbsolutas + " " + parseFloat(dataPopup[0]["PRECIO_PROMEDIO_ANTERIOR"]).toLocaleString("de-De").replace(",", ".") + '</span></p>';
+                HTML += '<p class="popup__list"><span class="popup__subtitle">Tendencia: </span><span class="popup__subtitle">' + dataPopup[0]["TENDENCIA"] + '</span></p>';
+                HTML += '<p class="popup__list"><span class="popup__subtitle">Variación: </span><span class="popup__subtitle">' + parseFloat(dataPopup[0]["VARIACION_PP"]).toLocaleString("de-De").replace(",", ".") + ' %</span></p>';
               }
             }
 
